4c8d26022e5aceaab17ce2207c463dd0d177485c
la <la>
2020-01-22 12:50:09 -0300

    casi listo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
 import Card from './componentes/playingcard/card.js';
+import Lara01 from './componentes/playingcard/img/tomb-raider.jpg';
+import Lara02 from './componentes/playingcard/img/lara02.jpg';
+import Lara03 from './componentes/playingcard/img/lara03.jpg';
+import Lara04 from './componentes/playingcard/img/lara04.jpg';
 
 const App = ()=> {
   return (
     <div>
-      <Card></Card> 
+      <Card cardImage={Lara01}
+            cardSubTitle="Lara del 2018"></Card> 
+      <Card cardImage={Lara02}
+            cardSubTitle="Lara del 2010"></Card> 
+      <Card cardImage={Lara03}
+            cardSubTitle="Lara del 2000"></Card> 
+      <Card cardImage={Lara04}
+            cardSubTitle="Lara del 1990"></Card> 
     </div>
   );
 }
--- src/componentes/playingcard/card.js
+++ src/componentes/playingcard/card.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import Lara from './img/tomb-raider.jpg';
 import './card.css';
 
 
-const Card = () =>{
+
+const Card = props =>{
     return(
         <section className="card" style={
-            {backgroundImage:`url(${Lara})`}
+            {backgroundImage:`url(${props.cardImage})`}
             }>
             
             <article className="card-article">
-                <h1>Powerful Women</h1>
+                <h1>{props.cardSubTitle}</h1>
                 <h2>Lara Croft</h2>
                 <p>This powerhouse of strength, smarts, and beauty is not to be trifled with. After the recent reboot of her 20-year-old video game franchise Tomb Raider, Lara is headed to the big screen next year for even greater adventures.</p>
                 <footer>
