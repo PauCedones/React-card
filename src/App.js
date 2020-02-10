d15af6a46e9f93cbf960d9c1e540ff5ed078d851
Paula <cedones@gmail.com>
2020-02-10 18:20:25 -0300

    agregado map y key, desaparecio la foto

--- src/App.js
+++ src/App.js
@@ -6,18 +6,49 @@ import Lara02 from './componentes/playingcard/img/lara02.jpg';
 import Lara03 from './componentes/playingcard/img/lara03.jpg';
 import Lara04 from './componentes/playingcard/img/lara04.jpg';
 
+
+const cards = [
+      {
+            cardAño: "Lara del 2018",
+            cardTitle: "Lara Croft",
+            cardFoto: Lara01,
+            cardParrafo: "This powerhouse of strength, smarts, and beauty is not to be trifled with. After the recent reboot of her 20-year-old video game franchise Tomb Raider, Lara is headed to the big screen next year for even greater adventures."
+      },
+      {
+            cardAño: "Lara del 2010",
+            cardTitle: "Lara Croft",
+            cardFoto: Lara02,
+            cardParrafo: "This powerhouse of strength, smarts, and beauty is not to be trifled with. After the recent reboot of her 20-year-old video game franchise Tomb Raider, Lara is headed to the big screen next year for even greater adventures."   
+      },
+      {
+            cardAño: "Lara del 2000",
+            cardTitle: "Lara Croft",
+            cardFoto: Lara03,
+            cardParrafo: "This powerhouse of strength, smarts, and beauty is not to be trifled with. After the recent reboot of her 20-year-old video game franchise Tomb Raider, Lara is headed to the big screen next year for even greater adventures."
+      },
+      {
+            cardAño: "Lara del 1990",
+            cardTitle: "Lara Croft",
+            cardFoto: 'Lara04',
+            cardParrafo: "This powerhouse of strength, smarts, and beauty is not to be trifled with. After the recent reboot of her 20-year-old video game franchise Tomb Raider, Lara is headed to the big screen next year for even greater adventures."
+      }
+
+];
+
+
+
 const App = ()=> {
   return (
     <div>
-      <Card cardImage={Lara01}
-            cardSubTitle="Lara del 2018"></Card> 
-      <Card cardImage={Lara02}
-            cardSubTitle="Lara del 2010"></Card> 
-      <Card cardImage={Lara03}
-            cardSubTitle="Lara del 2000"></Card> 
-      <Card cardImage={Lara04}
-            cardSubTitle="Lara del 1990"></Card> 
-    </div>
+      {cards.map(card => {
+      return(<Card key={card.cardTitle}
+                  cardAño={cards.cardAño}
+                  cardTitle={cards.cardTitle}
+                  cardFoto={cards.cardFoto}
+                  cardParrafo={cards.cardParrafo}></Card>); 
+      })}
+
+      </div>
   );
 }
 
